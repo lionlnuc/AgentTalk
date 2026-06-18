7e6571115f78c878f1a7b4aaacf142951584cecc
流云鹤 <15589352+lionmap@user.noreply.gitee.com>
2026-06-18 13:51:05 +0800

    搭建基础UI

---
+++ frontend/src/components/navbar/NavBar.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+import Menuicon from "@/components/navbar/icons/Menuicon.vue";
+import Homepageicon from "@/components/navbar/icons/Homepageicon.vue";
+import Friendicon from "@/components/navbar/icons/Friendicon.vue";
+import Createicon from "@/components/navbar/icons/Createicon.vue";
+import Searchicon from "@/components/navbar/icons/Searchicon.vue";
+</script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <nav class="navbar w-full bg-base-100 shadow-sm ">
+        <div class="navbar-start">
+          <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+            <Menuicon/>
+          </label>
+          <div class="px-2 font-bold text-xl" >Agent Talk</div>
+        </div>
+        <div class="navbar-center w-4/5 max-w-180">
+          <div class="join w-4/5">
+            <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
+            <button class="btn join-item rounded-r-full gap-0.5">
+              <Searchicon/>
+              搜索
+            </button>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <button class="btn btn-ghost text-lg" >登录</button>
+        </div>
+      </nav>
+      <slot></slot>
+    </div>
+
+    <div class="drawer-side is-drawer-close:overflow-visible">
+      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+      <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-54">
+        <ul class="menu w-full grow">
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="首页">
+              <homepageicon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">首页</span>
+            </button>
+          </li>
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="好友">
+              <Friendicon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">好友</span>
+            </button>
+          </li>
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="创作">
+              <Createicon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">创作</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
