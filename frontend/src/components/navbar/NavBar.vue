147a76bfb8725fcd6b5e4ae20ada494869206b3f
流云鹤 <15589352+lionmap@user.noreply.gitee.com>
2026-06-19 01:14:49 +0800

    实现路由

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -18,8 +18,8 @@ import Searchicon from "@/components/navbar/icons/Searchicon.vue";
           </label>
           <div class="px-2 font-bold text-xl" >Agent Talk</div>
         </div>
-        <div class="navbar-center w-4/5 max-w-180">
-          <div class="join w-4/5">
+        <div class="navbar-center w-4/5 max-w-180 flex justify-center">
+          <div class="join w-4/5 flex justify-center">
             <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
             <button class="btn join-item rounded-r-full gap-0.5">
               <Searchicon/>
@@ -28,7 +28,9 @@ import Searchicon from "@/components/navbar/icons/Searchicon.vue";
           </div>
         </div>
         <div class="navbar-end">
-          <button class="btn btn-ghost text-lg" >登录</button>
+          <RouterLink :to="{name: 'user-account-login-index'}" active-class="btn-focus" class="btn btn-ghost text-lg" >
+          登录
+          </RouterLink>
         </div>
       </nav>
       <slot></slot>
@@ -39,22 +41,22 @@ import Searchicon from "@/components/navbar/icons/Searchicon.vue";
       <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-54">
         <ul class="menu w-full grow">
           <li>
-            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="首页">
+            <RouterLink :to="{name: 'homepage-index'}" active-class="menu-focus" class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3"  data-tip="首页">
               <homepageicon />
               <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">首页</span>
-            </button>
+            </RouterLink>
           </li>
           <li>
-            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="好友">
+            <RouterLink :to="{name: 'friend-index'}" active-class="menu-focus" class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3"  data-tip="好友">
               <Friendicon />
               <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">好友</span>
-            </button>
+            </RouterLink>
           </li>
           <li>
-            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py=3"  data-tip="创作">
+            <RouterLink :to="{name: 'create-index'}" active-class="menu-focus" class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3"  data-tip="创作">
               <Createicon />
               <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">创作</span>
-            </button>
+            </RouterLink>
           </li>
         </ul>
       </div>
---
+++ frontend/src/views/create/CreateIndex.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+创造
+</template>
+
+<style scoped>
+
+</style>
