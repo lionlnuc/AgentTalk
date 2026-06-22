0b4ace31df6010059639df505577ec281064f288
流云鹤 <15589352+lionmap@user.noreply.gitee.com>
2026-06-22 13:07:46 +0800

    实现角色增删改查api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from web.models.user import UserProfile
+from web.models.character import Character
 # Register your models here.
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)#列表   逗号必须保留！！！为一个列表，查找时页面加载100条；若写成`raw_id_fields`,则添加用户时，名字为所有用户的下拉菜单
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,6 +1,11 @@
 from tkinter.font import names
 
 from django.urls import path,re_path
+
+from web.views.create.character.create import CreateCharacterView
+from web.views.create.character.get_single import GetSingleCharacterView
+from web.views.create.character.remove import RemoveCharacterView
+from web.views.create.character.update import UpdateCharacterView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
@@ -8,6 +13,7 @@ from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
 from web.views.user.profile.update import UpdateProfileView
+
 urlpatterns = [
     path('api/user/account/login/',LoginView.as_view()),#前加api为了与系统默认做区分
     path('api/user/account/logout/',LogoutView.as_view()),
@@ -15,6 +21,11 @@ urlpatterns = [
     path('api/user/account/refresh_token/',RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/',GetUserInfoView.as_view()),
     path('api/user/profile/update/',UpdateProfileView.as_view()),
+    path('api/create/character/create/', CreateCharacterView.as_view()),
+    path('api/create/character/update/', UpdateCharacterView.as_view()),
+    path('api/create/character/remove/', RemoveCharacterView.as_view()),
+    path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+
     path('', index),
     re_path(r'^(?!media/|static/|assets/).*$', index)#前端
 ]
