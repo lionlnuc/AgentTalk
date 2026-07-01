dbde96eb3c03e7ac7ca2391e91a713395c33c087
流云鹤 <15589352+lionmap@user.noreply.gitee.com>
2026-07-01 23:47:48 +0800

    实现知识库

---
+++ backend/web/documents/utils/insert_documents.py
@@ -0,0 +1,27 @@
+import lancedb
+from django.db import connection
+from lancedb import connect
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import LanceDB
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from web.documents.utils.custom_embeddings import CustomEmbeddings
+
+
+def insert_documents():
+    loader = TextLoader('./web/documents/data.txt',encoding='utf-8')#读取器
+    documents = loader.load() #读取文档
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=500) #文档切分器
+    texts = text_splitter.split_documents(documents)
+    print(f"已切分成 {len(texts)} 个片段。")
+
+    embeddings = CustomEmbeddings()
+    db = lancedb.connect('./web/documents/lancedb_storage')#连接数据库
+    vector_db = LanceDB.from_documents(#向量数据库
+        documents = texts,
+        embedding = embeddings,
+        connection = db,
+        table_name = 'my_knowledge_base',
+        mode = 'overwrite',
+    )
+    print(f"已插入 {vector_db._table.count_rows()} 行数据")
